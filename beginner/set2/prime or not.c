556e86e34bd741fc0df68307e27570fcba28eb54
ragulasakthivel <34960334+ragulasakthivel@users.noreply.github.com>
2018-01-04 22:10:28 +0530

    Create prime or not.c

---
+++ beginner/set2/prime or not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+    int n,count=0,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+       if(n%i==0)   
+        count++;
+    }
+    if(count==2)
+    printf("yes...prime number");
+    else
+    printf("no...not a prime number ");
+}
